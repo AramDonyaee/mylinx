60443881622c1bb1a59b3eb143066d7990ad48d7
AramDonyaee <adonyaee@yahoo.com>
2023-05-05 20:19:51 +0330

    building in local

---
+++ public/build/assets/Links-d1a41861.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./AppLayout-9d0a67c0.js";import{c as o,o as r}from"./app-ab2e264b.js";import"./ApplicationMark-d108d7ec.js";import"./_plugin-vue_export-helper-c27b6911.js";const n={__name:"Links",setup(e){return(s,_)=>(r(),o(t,{title:"Links"}))}};export{n as default};
