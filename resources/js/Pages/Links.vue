278345dac45e295541e1956f6d7fd26bafd3c490
AramDonyaee <adonyaee@yahoo.com>
2023-05-05 18:03:48 +0330

    fixing some bugs

--- resources/js/Pages/Links.vue
+++ resources/js/Pages/Links.vue
@@ -1,12 +1,11 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Links from '@/Components/Links.vue';
-import LinksExperiment from '@/Components/LinksExperiment.vue';
+
 
 </script>
 
 <template>
     <AppLayout title="Links">
-        <Links />
+
     </AppLayout>
 </template>
