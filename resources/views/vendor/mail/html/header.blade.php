3a15ca2d39e916891d3eabe5c8a7c77e7420ad63
AramDonyaee <adonyaee@yahoo.com>
2023-08-03 21:12:22 +0330

    fixing email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Mylinx')
-<img src="{{url('/logo.png')}}" class="logo" alt="Mylinx logo">
+<img src="{{public_path('logo.png')}}" class="logo" alt="Mylinx logo">
 @else
 {{ $slot }}
 @endif
