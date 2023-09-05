19a5a7eb6528840418228be065bf65a334f990b7
AramDonyaee <adonyaee@yahoo.com>
2023-09-06 00:26:23 +0330

    adding microsoft clarity analytics tool

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,6 +32,18 @@
         <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon" />
 
         <script src="https://cdn.tailwindcss.com"></script>
+        <script type="text/javascript">
+            (function(c, l, a, r, i, t, y) {
+                c[a] = c[a] || function() {
+                    (c[a].q = c[a].q || []).push(arguments)
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "iqkzz56ita");
+        </script>
         <style>
             #txt {
                 border: none;
