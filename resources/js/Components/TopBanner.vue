a856b6d7fe535396406b5bb8c659dbec0a1c508e
AramDonyaee <adonyaee@yahoo.com>
2023-09-08 17:54:57 +0330

    changin top banner text

--- resources/js/Components/TopBanner.vue
+++ resources/js/Components/TopBanner.vue
@@ -2,7 +2,7 @@
 	<div class="px-8 py-2 bg-gradient-to-r from-purple-500 to-pink-500 text-white dark:bg-gray-900 dark:text-gray-100">
 		<div class="flex items-center mx-auto container justify-center py-2">
 			<div>
-				<span class="text-sm sm:text-base">Mylinx pro is <span class="font-bold">free</span> until the end of 2023!</span>
+				<span class="text-sm sm:text-base"><span class="font-bold">Mylinx pro</span> is free for ever to all our early users until the end of 2023!</span>
 			</div>
 		</div>
 	</div>
